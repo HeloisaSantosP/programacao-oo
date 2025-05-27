c3907bf437ad38ca66ffc7d6bd71a92dd6ce32eb
HeloisaSantosP <heloisa.pagliari@utp.edu.br>
2025-05-27 20:45:15 -0300

    Create Cliente.java

---
+++ ed03/Sistema de Login/Cliente.java
@@ -0,0 +1,9 @@
+class Cliente extends Usuario implements Autenticavel {
+    public Cliente(String login, String senha) {
+        super(login, senha);
+    }
+
+    public boolean autenticar(String senha) {
+        return senha.equals(this.senha) && senha.length() > 6;
+    }
+}
