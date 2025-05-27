af5133ccd9b9b1b34faafa8f6d00e0ea0d7fcc22
HeloisaSantosP <heloisa.pagliari@utp.edu.br>
2025-05-27 20:02:35 -0300

    Create FuncionarioTemporario.java

---
+++ ed03/FuncionarioTemporario.java
@@ -0,0 +1,11 @@
+class FuncionarioTemporario extends Funcionario {
+
+    public FuncionarioTemporario(String nome, double salarioBase) {
+        super(nome, salarioBase);
+    }
+
+    @Override
+    public double calcularSalario() {
+        return salarioBase - 100;
+    }
+}
