ce9b59da518e4a89f173544db3ddf0a79d9c30e5
HeloisaSantosP <heloisa.pagliari@utp.edu.br>
2025-05-27 20:41:20 -0300

    Create Main.java

---
+++ ed03/Sistema de Login/Main.java
@@ -0,0 +1,24 @@
+class Main {
+    public static void main(String[] args) {
+        
+        Administrador admin = new Administrador("admin", "admin123");
+        admin.exibirLogin();
+        System.out.println("Autenticação com 'admin123': " + admin.autenticar("admin123"));
+        System.out.println("Autenticação com 'errada': " + admin.autenticar("errada"));
+        admin.alterarSenha("novaSenha123");
+        admin.bloquearUsuario();
+        admin.registrarLog("Exclusão de conta");
+
+        System.out.println();
+
+        Visitante guest = new Visitante("guest", "qualquer");
+        guest.exibirLogin();
+
+        System.out.println();
+
+        Cliente cliente = new Cliente("joao", "segredo123");
+        cliente.exibirLogin();
+        System.out.println("Autenticação com 'segredo123': " + cliente.autenticar("segredo123"));
+        System.out.println("Autenticação com '12345': " + cliente.autenticar("12345"));
+    }
+}
